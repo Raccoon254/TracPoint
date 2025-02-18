dd5f62d082b1041fb6c76988f4ae65bd264ddd0d
Raccoon254 <tomsteve187@gmail.com>
2025-02-18 14:02:57 +0300

    -- init -- start on the user dashboard

--- app/Livewire/Dashboards/UserDashboard.php
+++ app/Livewire/Dashboards/UserDashboard.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire\Dashboards;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class UserDashboard extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.dashboards.user-dashboard');
     }
