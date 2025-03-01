ed5e8ee84db4587cf784e60aa99fe8b13b02acff
Raccoon254 <tomsteve187@gmail.com>
2025-03-01 10:39:17 +0300

    -- create -- start an audit page

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('reports', \App\Livewire\reports\Index::class)->name('reports.index');
     Route::get('settings', \App\Livewire\settings\Index::class)->name('settings.index');
 
+    Route::get('audits/create', \App\Livewire\audits\Create::class)->name('audits.create');
+
     // Notifications
     Route::get('notifications', \App\Livewire\Notifications\ShowAll::class)->name('notifications.index');
 });
